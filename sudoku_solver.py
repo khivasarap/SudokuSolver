c31f9d9356b5de64cf46db237a119de35967b02f
Modak <info@modak.us>
2020-10-13 14:40:21 -0700

    Magically put the output

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,14 +1,35 @@
-grid = [
-[4,0,0,0,0,5,0,0,0],
-[0,0,0,0,0,0,1,9,8],
-[3,0,0,0,8,2,4,0,0],
-[0,0,0,1,0,0,0,8,0],
-[9,0,3,0,0,0,0,0,0],
-[0,0,0,0,3,0,6,7,0],
-[0,5,0,0,0,9,0,0,0],
-[0,0,0,2,0,0,9,0,7],
-[6,4,0,3,0,0,0,0,0]]
+#References: https://pyautogui.readthedocs.io/en/latest/
 
+import pyautogui as pg
+import numpy
+import time
+# grid = [
+# [4,0,0,0,0,5,0,0,0],
+# [0,0,0,0,0,0,1,9,8],
+# [3,0,0,0,8,2,4,0,0],
+# [0,0,0,1,0,0,0,8,0],
+# [9,0,3,0,0,0,0,0,0],
+# [0,0,0,0,3,0,6,7,0],
+# [0,5,0,0,0,9,0,0,0],
+# [0,0,0,2,0,0,9,0,7],
+# [6,4,0,3,0,0,0,0,0]]
+grid = []
+
+#input as rows
+while True:
+    row = list(input('Row: '))
+    ints = []
+
+    for n in row:
+        ints.append(int(n))
+    grid.append(ints)
+
+    if len(grid) == 9:
+        break
+    print('Row ' + str(len(grid)) + ' Complete')
+
+# To ensure you can click the first box in https://sudoku.com/
+time.sleep(2)
 # defines row and column position of number n in the grid
 def possible_axis(row, column, number):
     #Check if exists in row
@@ -36,10 +57,32 @@ def possible_box(row,column,number):
     
     return True
 
+# Displays the output on https://sudoku.com/
 def display(matrix):
+    final = []
+    string_final = []
+    for i in range(9):
+        final.append(matrix[i])
+
+    for lists in final:
+        for num in lists:
+            string_final.append(str(num))
+
+    counter=[]
+    for num in string_final:
+        pg.press(num)
+        pg.hotkey('right')
+        counter.append(num)
+        # We are at end of row
+        if len(counter)%9 ==0:
+            pg.hotkey('down')
+            # Go left
+            for i in range(8):
+                pg.hotkey('left')
+
+def display_cmd(matrix):
     for i in range(9):
         print(matrix[i])
-
 
 # Solve using Backtracking. Place the number in the box and check if valid.
 def solve_backtrack():
@@ -57,6 +100,6 @@ def solve_backtrack():
                         grid[x][y] = 0
                 return
     display(grid)
- 
+    display_cmd(grid) 
 
 solve_backtrack()
